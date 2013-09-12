5fe77cbf280f360e71f5ba65744634f45b294c2a
jambrose42 <jambrose@JamAir.local>
2013-09-12 01:12:13 -0400

    Hello Slim!
    Getting Slim installed and running.  Pretty neat!

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
- <?php 
- 
- echo 'Hello PHP!';
- 
- phpinfo(); 
-	 
- ?>
+<?php
+
+require 'lib/Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+
+$app->run();
+
+?>

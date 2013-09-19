f46fabbf639d55998bf3f0b9c37c71eba3fc3bc2
jambrose42 <jambrose@JamAir.local>
2013-09-19 19:55:21 -0400

    Remove a redundant else

--- lib/Model/Location.php
+++ lib/Model/Location.php
@@ -18,78 +18,76 @@ class Location {
 			
 		    throw new Exception('Error trying to connect to mysql: ' . $mysqli->connect_error);
 		
+		}
+		
+		$query = "SELECT * FROM `locations` WHERE id = " . $id;
+	
+		$result = $mysqli->query($query);
+		
+		if (!$result) {
+			
+			die("Invalid query: " . $mysqli->error);
+		
 		} else {
+			
+			$obj = $result->fetch_object();
+			
+			// add some other handy version of the name
+			
+			$obj->shortname = str_replace(' ', '', $obj->name);
+			$urlName = str_replace(' ', '-', $obj->name);
+			$obj->urlName = str_replace('.', '', $urlName);
+			
+			// create array of amenities
+			
+			$i = 0;
+			
+			while ($finfo = $result->fetch_field()) {
 		
-			$query = "SELECT * FROM `locations` WHERE id = " . $id;
+		        $amenityName = $finfo->name;
+		                
+		        // if a field starts with 'has'        
+		                
+		        if (substr($amenityName, 0, 3) == "has") {
+		        
+		        	// if that field has a value of one, let's grab that amenity name
+		        	
+		        	if ($obj->$amenityName == 1) {
+			        	
+			        	// strip 'has' off field name
+		        	
+						$amenityName = substr($amenityName, 3);
+													
+						// turn camel case into separate words
+						
+						$pattern = '/(.*?[a-z]{1})([A-Z]{1}.*?)/';
+						$replace = '${1} ${2}'; 
+						$amenity = preg_replace($pattern, $replace, $amenityName);
 		
-			$result = $mysqli->query($query);
-			
-			if (!$result) {
-				
-				die("Invalid query: " . $mysqli->error);
-			
-			} else {
-				
-				$obj = $result->fetch_object();
-				
-				// add some other handy version of the name
-				
-				$obj->shortname = str_replace(' ', '', $obj->name);
-				$urlName = str_replace(' ', '-', $obj->name);
-				$obj->urlName = str_replace('.', '', $urlName);
-				
-				// create array of amenities
-				
-				$i = 0;
-				
-				while ($finfo = $result->fetch_field()) {
-			
-			        $amenityName = $finfo->name;
-			                
-			        // if a field starts with 'has'        
-			                
-			        if (substr($amenityName, 0, 3) == "has") {
+						// add the amenity to a handy array
 			        
-			        	// if that field has a value of one, let's grab that amenity name
-			        	
-			        	if ($obj->$amenityName == 1) {
-				        	
-				        	// strip 'has' off field name
-			        	
-							$amenityName = substr($amenityName, 3);
-														
-							// turn camel case into separate words
-							
-							$pattern = '/(.*?[a-z]{1})([A-Z]{1}.*?)/';
-							$replace = '${1} ${2}'; 
-							$amenity = preg_replace($pattern, $replace, $amenityName);
+			    	    $amenities[$i] = $amenity;
+			    	    
+			    	    $i++;
+			    	    
+		        	}
+			        
+		        }
+		    
+		    }
 			
-							// add the amenity to a handy array
-				        
-				    	    $amenities[$i] = $amenity;
-				    	    
-				    	    $i++;
-				    	    
-			        	}
-				        
-			        }
-			    
-			    }
+			// add the amenity array to the object
+			
+			$obj->amenities = $amenities;
 				
-				// add the amenity array to the object
-				
-				$obj->amenities = $amenities;
-					
-				/* free result set */
-				$result->close();	
-			
-			}
-		
-			// close connection
-			
-			$mysqli->close();
+			/* free result set */
+			$result->close();	
 		
 		}
+	
+		// close connection
+		
+		$mysqli->close();
 		
 		return $obj;	
 		
